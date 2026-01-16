29aaf7d2c15f2556aabd3641bef692aa4487472f
PepeBumblebee-meep <gigachadpepebumblebee@gmail.com>
2026-01-16 21:41:54 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,15 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./**/*.{js,ts,jsx,tsx,mdx}",
+    "./**/*.{js,ts,jsx,tsx,mdx}" // <--- Dòng này là "Bùa hộ mệnh", nó quét mọi ngóc ngách
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
 export default config;
